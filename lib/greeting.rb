6f0ec7e364d3534075937f9193928119eb769610
Benjamin <benjaminrowe0@gmail.com>
2022-04-03 12:52:41 +1000

    done

---
+++ lib/greeting.rb
@@ -0,0 +1,15 @@
+#defi say_hello
+def say_hello
+    puts "Hello World!"
+end
+
+
+#calling the say_hello method 10 times
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
+say_hello
